dd17fab375b6bdbd04e0b3721ea21a95b6a3b91f
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-10-19 19:17:30 +0200

    create CoinsViewModel.kt

---
+++ app/src/main/java/com/abanoub/caching/ui/view/main/CoinsViewModel.kt
@@ -0,0 +1,29 @@
+package com.abanoub.caching.ui.view.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abanoub.caching.data.remote.api.CoinApi
+import com.abanoub.caching.data.remote.model.Coin
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinsViewModel @Inject constructor(
+    api: CoinApi
+) : ViewModel() {
+    private val coinsLiveData = MutableLiveData<List<Coin>>()
+    val coins: LiveData<List<Coin>> = coinsLiveData
+
+    init {
+        viewModelScope.launch {
+            val coins = api.getCoins()
+            delay(2000)
+            coinsLiveData.value = coins
+        }
+    }
+
+}
